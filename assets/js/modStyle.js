6d2875d92780a124c29975dcfce26d78e30cf04c
stiffynavales <stiffynavales@gmail.com>
2021-10-12 17:30:07 +0800

    typed text

---
+++ assets/js/modStyle.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".title", {
+    strings: ["System Administrator", "Web Developer", "Tech Support Engr.", "Photo Editor", "IT Specialist"],
+    typeSpeed: 150,
+    backSpeed: 60,
+    loop: true
+});
