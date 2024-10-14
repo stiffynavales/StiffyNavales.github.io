af7f096b11479455ebe507bd19b71e2fd52e3b42
Stiffy Navales <56945423+stiffynavales@users.noreply.github.com>
2024-10-14 10:41:22 +0800

    Update modStyle.js

--- assets/js/modStyle.js
+++ assets/js/modStyle.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".title", {
-    strings: ["System Administrator", "Web Developer", "Tech Support Engr.", "Photo Editor", "IT Specialist"],
+    strings: ["Web Designer", "Graphics Designer", "Video Editor", "Photo Editor", "IT Specialist"],
     typeSpeed: 150,
     backSpeed: 60,
     loop: true
